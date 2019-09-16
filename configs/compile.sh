a30ab2dc0fc22707113d5de5574943cdc2fb4992
McLurch <54000610+McLurch@users.noreply.github.com>
2019-09-16 18:53:02 -0500

    Create compile.sh

---
+++ configs/compile.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+# Make Directories
+mkdir /var/software/fivem/fivem-server-default/configs/esx_accessories
+mkdir /var/software/fivem/fivem-server-default/configs/esx_advanced_holdup
+mkdir /var/software/fivem/fivem-server-default/configs/esx_advancedfishing
+mkdir /var/software/fivem/fivem-server-default/configs/esx_advancedgarage
+mkdir /var/software/fivem/fivem-server-default/configs/esx_aiomenu
+mkdir /var/software/fivem/fivem-server-default/configs/esx_aircargojob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_aircraftshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_airlines
+mkdir /var/software/fivem/fivem-server-default/configs/esx_ambulancejob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_animations
+mkdir /var/software/fivem/fivem-server-default/configs/esx_armyjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_atm
+mkdir /var/software/fivem/fivem-server-default/configs/esx_bankerjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_barbershop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_billing
+mkdir /var/software/fivem/fivem-server-default/configs/esx_boatshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_burglary
+mkdir /var/software/fivem/fivem-server-default/configs/esx_carthief
+mkdir /var/software/fivem/fivem-server-default/configs/esx_carwash
+mkdir /var/software/fivem/fivem-server-default/configs/esx_clotheshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_cruisecontrol
+mkdir /var/software/fivem/fivem-server-default/configs/esx_dmvschool
+mkdir /var/software/fivem/fivem-server-default/configs/esx_dockshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_drugeffects
+mkdir /var/software/fivem/fivem-server-default/configs/esx_drugs
+mkdir /var/software/fivem/fivem-server-default/configs/esx_duty
+mkdir /var/software/fivem/fivem-server-default/configs/esx_eden_garage
+mkdir /var/software/fivem/fivem-server-default/configs/esx_fbijob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_firejob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_garage
+mkdir /var/software/fivem/fivem-server-default/configs/esx_hitmanjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_holdup
+mkdir /var/software/fivem/fivem-server-default/configs/esx_holdupbank
+mkdir /var/software/fivem/fivem-server-default/configs/esx_hospital
+mkdir /var/software/fivem/fivem-server-default/configs/esx_inventoryhud
+mkdir /var/software/fivem/fivem-server-default/configs/esx_jb_eden_garage2
+mkdir /var/software/fivem/fivem-server-default/configs/esx_joblisting
+mkdir /var/software/fivem/fivem-server-default/configs/esx_jobs
+mkdir /var/software/fivem/fivem-server-default/configs/esx_ktackle
+mkdir /var/software/fivem/fivem-server-default/configs/esx_licenseshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_lscustom
+mkdir /var/software/fivem/fivem-server-default/configs/esx_mafiajob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_masks
+mkdir /var/software/fivem/fivem-server-default/configs/esx_mechanicjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_moneywash
+mkdir /var/software/fivem/fivem-server-default/configs/esx_nightclubjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_optionalneeds
+mkdir /var/software/fivem/fivem-server-default/configs/esx_phone
+mkdir /var/software/fivem/fivem-server-default/configs/esx_planting
+mkdir /var/software/fivem/fivem-server-default/configs/esx_plasticsurgery
+mkdir /var/software/fivem/fivem-server-default/configs/esx_policejob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_property
+mkdir /var/software/fivem/fivem-server-default/configs/esx_realestateagentjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_repairkit
+mkdir /var/software/fivem/fivem-server-default/configs/esx_shops
+mkdir /var/software/fivem/fivem-server-default/configs/esx_skin
+mkdir /var/software/fivem/fivem-server-default/configs/esx_society
+mkdir /var/software/fivem/fivem-server-default/configs/esx_status
+mkdir /var/software/fivem/fivem-server-default/configs/esx_taxijob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_thelostmcjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_thief
+mkdir /var/software/fivem/fivem-server-default/configs/esx_truckshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_unicornjob
+mkdir /var/software/fivem/fivem-server-default/configs/esx_vehicleshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_voice
+mkdir /var/software/fivem/fivem-server-default/configs/esx_weaponshop
+mkdir /var/software/fivem/fivem-server-default/configs/esx_whitelist
+mkdir /var/software/fivem/fivem-server-default/configs/esx_whitelistEnhanced
+mkdir /var/software/fivem/fivem-server-default/configs/cv_control
+mkdir /var/software/fivem/fivem-server-default/configs/eden_accessories
+mkdir /var/software/fivem/fivem-server-default/configs/eden_animal
+mkdir /var/software/fivem/fivem-server-default/configs/FRFuel
+mkdir /var/software/fivem/fivem-server-default/configs/LegacyFuel
+mkdir /var/software/fivem/fivem-server-default/configs/Peds
+mkdir /var/software/fivem/fivem-server-default/configs/radargun
+mkdir /var/software/fivem/fivem-server-default/configs/skinchanger
+mkdir /var/software/fivem/fivem-server-default/configs/StreetRaces
+mkdir /var/software/fivem/fivem-server-default/configs/RealisticVehicleFailure
+mkdir /var/software/fivem/fivem-server-default/configs/es_extended
+mkdir /var/software/fivem/fivem-server-default/configs/essentialmode
+
+mkdir /var/software/fivem/fivem-server-default/configs/ServerPassword
+
+
+# Compile config files
+cp [esx]/esx_accessories/config.lua /var/software/fivem/fivem-server-default/configs/esx_accessories/config.lua
+cp [esx]/esx_advanced_holdup/config.lua /var/software/fivem/fivem-server-default/configs/esx_advanced_holdup/config.lua
+cp [esx]/esx_advancedfishing/config.lua /var/software/fivem/fivem-server-default/configs/esx_advancedfishing/config.lua
+cp [esx]/esx_advancedgarage/config.lua /var/software/fivem/fivem-server-default/configs/esx_advancedgarage/config.lua
+cp [esx]/esx_aiomenu/config.lua /var/software/fivem/fivem-server-default/configs/esx_aiomenu/config.lua
+cp [esx]/esx_aircargojob/config.lua /var/software/fivem/fivem-server-default/configs/esx_aircargojob/config.lua
+cp [esx]/esx_aircraftshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_aircraftshop/config.lua
+cp [esx]/esx_airlines/config.lua /var/software/fivem/fivem-server-default/configs/esx_airlines/config.lua
+cp [esx]/esx_ambulancejob/config.lua /var/software/fivem/fivem-server-default/configs/esx_ambulancejob/config.lua
+cp [esx]/esx_animations/config.lua /var/software/fivem/fivem-server-default/configs/esx_animations/config.lua
+cp [esx]/esx_armyjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_armyjob/config.lua
+cp [esx]/esx_atm/config.lua /var/software/fivem/fivem-server-default/configs/esx_atm/config.lua
+cp [esx]/esx_bankerjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_bankerjob/config.lua
+cp [esx]/esx_barbershop/config.lua /var/software/fivem/fivem-server-default/configs/esx_barbershop/config.lua
+cp [esx]/esx_billing/config.lua /var/software/fivem/fivem-server-default/configs/esx_billing/config.lua
+cp [esx]/esx_boatshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_boatshop/config.lua
+cp [esx]/esx_burglary/config.lua /var/software/fivem/fivem-server-default/configs/esx_burglary/config.lua
+cp [esx]/esx_carthief/config.lua /var/software/fivem/fivem-server-default/configs/esx_carthief/config.lua
+cp [esx]/esx_carwash/config.lua /var/software/fivem/fivem-server-default/configs/esx_carwash/config.lua
+cp [esx]/esx_clotheshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_clotheshop/config.lua
+cp [esx]/esx_cruisecontrol/config.lua /var/software/fivem/fivem-server-default/configs/esx_cruisecontrol/config.lua
+cp [esx]/esx_dmvschool/config.lua /var/software/fivem/fivem-server-default/configs/esx_dmvschool/config.lua
+cp [esx]/esx_dockshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_dockshop/config.lua
+cp [esx]/esx_drugeffects/config.lua /var/software/fivem/fivem-server-default/configs/esx_drugeffects/config.lua
+cp [esx]/esx_drugs/config.lua /var/software/fivem/fivem-server-default/configs/esx_drugs/config.lua
+cp [esx]/esx_duty/config.lua /var/software/fivem/fivem-server-default/configs/esx_duty/config.lua
+cp [esx]/esx_eden_garage/config.lua /var/software/fivem/fivem-server-default/configs/esx_eden_garage/config.lua
+cp [esx]/esx_fbijob/config.lua /var/software/fivem/fivem-server-default/configs/esx_fbijob/config.lua
+cp [esx]/esx_firejob/config.lua /var/software/fivem/fivem-server-default/configs/esx_firejob/config.lua
+cp [esx]/esx_garage/config.lua /var/software/fivem/fivem-server-default/configs/esx_garage/config.lua
+cp [esx]/esx_hitmanjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_hitmanjob/config.lua
+cp [esx]/esx_holdup/config.lua /var/software/fivem/fivem-server-default/configs/esx_holdup/config.lua
+cp [esx]/esx_holdupbank/config.lua /var/software/fivem/fivem-server-default/configs/esx_holdupbank/config.lua
+cp [esx]/esx_hospital/config.lua /var/software/fivem/fivem-server-default/configs/esx_hospital/config.lua
+cp [esx]/esx_inventoryhud/config.lua /var/software/fivem/fivem-server-default/configs/esx_inventoryhud/config.lua
+cp [esx]/esx_jb_eden_garage2/config.lua /var/software/fivem/fivem-server-default/configs/esx_jb_eden_garage2/config.lua
+cp [esx]/esx_joblisting/config.lua /var/software/fivem/fivem-server-default/configs/esx_joblisting/config.lua
+cp [esx]/esx_jobs/config.lua /var/software/fivem/fivem-server-default/configs/esx_jobs/config.lua
+cp [esx]/esx_ktackle/config.lua /var/software/fivem/fivem-server-default/configs/esx_ktackle/config.lua
+cp [esx]/esx_licenseshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_licenseshop/config.lua
+cp [esx]/esx_lscustom/config.lua /var/software/fivem/fivem-server-default/configs/esx_lscustom/config.lua
+cp [esx]/esx_mafiajob/config.lua /var/software/fivem/fivem-server-default/configs/esx_mafiajob/config.lua
+cp [esx]/esx_masks/config.lua /var/software/fivem/fivem-server-default/configs/esx_masks/config.lua
+cp [esx]/esx_mechanicjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_mechanicjob/config.lua
+cp [esx]/esx_moneywash/config.lua /var/software/fivem/fivem-server-default/configs/esx_moneywash/config.lua
+cp [esx]/esx_nightclubjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_nightclubjob/config.lua
+cp [esx]/esx_optionalneeds/config.lua /var/software/fivem/fivem-server-default/configs/esx_optionalneeds/config.lua
+cp [esx]/esx_phone/config.lua /var/software/fivem/fivem-server-default/configs/esx_phone/config.lua
+cp [esx]/esx_planting/config.lua /var/software/fivem/fivem-server-default/configs/esx_planting/config.lua
+cp [esx]/esx_plasticsurgery/config.lua /var/software/fivem/fivem-server-default/configs/esx_plasticsurgery/config.lua
+cp [esx]/esx_policejob/config.lua /var/software/fivem/fivem-server-default/configs/esx_policejob/config.lua
+cp [esx]/esx_property/config.lua /var/software/fivem/fivem-server-default/configs/esx_property/config.lua
+cp [esx]/esx_realestateagentjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_realestateagentjob/config.lua
+cp [esx]/esx_repairkit/config.lua /var/software/fivem/fivem-server-default/configs/esx_repairkit/config.lua
+cp [esx]/esx_shops/config.lua /var/software/fivem/fivem-server-default/configs/esx_shops/config.lua
+cp [esx]/esx_skin/config.lua /var/software/fivem/fivem-server-default/configs/esx_skin/config.lua
+cp [esx]/esx_society/config.lua /var/software/fivem/fivem-server-default/configs/esx_society/config.lua
+cp [esx]/esx_status/config.lua /var/software/fivem/fivem-server-default/configs/esx_status/config.lua
+cp [esx]/esx_taxijob/config.lua /var/software/fivem/fivem-server-default/configs/esx_taxijob/config.lua
+cp [esx]/esx_thelostmcjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_thelostmcjob/config.lua
+cp [esx]/esx_thief/config.lua /var/software/fivem/fivem-server-default/configs/esx_thief/config.lua
+cp [esx]/esx_truckshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_truckshop/config.lua
+cp [esx]/esx_unicornjob/config.lua /var/software/fivem/fivem-server-default/configs/esx_unicornjob/config.lua
+cp [esx]/esx_vehicleshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_vehicleshop/config.lua
+cp [esx]/esx_voice/config.lua /var/software/fivem/fivem-server-default/configs/esx_voice/config.lua
+cp [esx]/esx_weaponshop/config.lua /var/software/fivem/fivem-server-default/configs/esx_weaponshop/config.lua
+cp [esx]/esx_whitelist/config.lua /var/software/fivem/fivem-server-default/configs/esx_whitelist/config.lua
+cp [esx]/esx_whitelistEnhanced/config.lua /var/software/fivem/fivem-server-default/configs/esx_whitelistEnhanced/config.lua
+cp [mods]/cv_control/gui.lua /var/software/fivem/fivem-server-default/configs/cv_control/gui.lua
+cp [mods]/eden_accessories/config.lua /var/software/fivem/fivem-server-default/configs/eden_accessories/config.lua
+cp [mods]/eden_animal/config.lua /var/software/fivem/fivem-server-default/configs/eden_animal/config.lua
+cp [mods]/FRFuel/config.lua /var/software/fivem/fivem-server-default/configs/FRFuel/config.lua
+cp [mods]/LegacyFuel/config.lua /var/software/fivem/fivem-server-default/configs/LegacyFuel/config.lua
+cp [mods]/Peds/config.lua /var/software/fivem/fivem-server-default/configs/Peds/config.lua
+cp [mods]/radargun/config.lua /var/software/fivem/fivem-server-default/configs/radargun/config.lua
+cp [mods]/skinchanger/config.lua /var/software/fivem/fivem-server-default/configs/skinchanger/config.lua
+cp [mods]/StreetRaces/config.lua /var/software/fivem/fivem-server-default/configs/StreetRaces/config.lua
+cp [mods]/RealisticVehicleFailure/config.lua /var/software/fivem/fivem-server-default/configs/RealisticVehicleFailure/config.lua
+cp [essential]/es_extended/config.lua /var/software/fivem/fivem-server-default/configs/es_extended/config.lua
+cp essentialmode/config.lua /var/software/fivem/fivem-server-default/configs/essentialmode/config.lua
+
+cp [mods]/ServerPassword/server.lua /var/software/fivem/fivem-server-default/configs/ServerPassword/server.lua
